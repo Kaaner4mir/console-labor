698fbb29f3ca27dc6ca51e752bcaf1a4898cbb4c
Kaaner <kaaner4mir@gmail.com>
2025-11-01 19:12:42 +0300

    refactor: redesign listing method

--- AtmSimulation/Operation/ShowBalance.cs
+++ AtmSimulation/Operation/ShowBalance.cs
@@ -1,15 +1,27 @@
 ﻿class ShowBalance
 {
-    public static void Show()
+    public static void ListAccount()
     {
         foreach (var item in Data._accounts)
         {
-            ConsoleManager.WriteColored($"🔑 Account ID   : {item.AccountId}",ConsoleColor.Yellow);
-            ConsoleManager.WriteColored($"🏷️ Account Name : {item.AccountName}",ConsoleColor.White);
-            ConsoleManager.WriteColored($"🏦 Branch       : {item.Branch}",ConsoleColor.Blue);
-            ConsoleManager.WriteColored($"🗓️ Opening Date : {item.OpeningDate}",ConsoleColor.Cyan);
+            ConsoleManager.WriteColored($"🔑 Account ID   : {item.AccountId}", ConsoleColor.Yellow);
+            ConsoleManager.WriteColored($"🏷️ Account Name : {item.AccountName}", ConsoleColor.White);
+            ConsoleManager.WriteColored($"🏦 Branch       : {item.Branch}", ConsoleColor.Blue);
+            ConsoleManager.WriteColored($"🗓️ Opening Date : {item.OpeningDate}", ConsoleColor.Cyan);
             ConsoleManager.WriteColored($"💱 Currency     : {item.Currency}", ConsoleColor.Gray);
-            ConsoleManager.WriteColored($"🪙 Balance      : {item.Currency}{item.Balance}",ConsoleColor.Green);
+            ConsoleManager.WriteColored($"🪙 Balance      : {item.Currency}{item.Balance}", ConsoleColor.Green);
+
+            ConsoleManager.WriteColored(new string('-', 40));
+        }
+    }
+
+    public static void ListForeignAccount()
+    {
+        foreach (var item in Data._foreignaccounts)
+        {
+            ConsoleManager.WriteColored($"🔑 Account ID   : {item.AccountId}", ConsoleColor.Yellow);
+            ConsoleManager.WriteColored($"🏦 Branch       : {item.Branch}", ConsoleColor.Blue);
+            ConsoleManager.WriteColored($"💱 Currency     : {item.Currency}", ConsoleColor.Gray);
 
             ConsoleManager.WriteColored(new string('-', 40));
         }
