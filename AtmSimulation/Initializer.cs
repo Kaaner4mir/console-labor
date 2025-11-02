6820f3e71a5df9a2d3efa93c4e095a8f3595166b
Kaaner <kaaner4mir@gmail.com>
2025-11-02 18:53:19 +0300

    feat: add switch case

--- AtmSimulation/Initializer.cs
+++ AtmSimulation/Initializer.cs
@@ -23,6 +23,7 @@ class Initializer
                     case 2: WithdrawMoney.Withdraw(); break;
                     case 3: DepositMoney.Deposit(); break;
                     case 4: MoneyTransfer.Transfer(); break;
+                    case 5: BillPayment.Payment(); break;   
                     case 6: ShowLog.Show(); break;
                     default: ConsoleManager.WriteColored("\n❌ The transaction you want to make is invalid! Please try again."); break;
                 }
