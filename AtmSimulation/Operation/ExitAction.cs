a8278597322a19601f3a6565e7646492f129aa98
Kaaner <kaaner4mir@gmail.com>
2025-11-03 11:22:50 +0300

    feat: add exit function

---
+++ AtmSimulation/Operation/ExitAction.cs
@@ -0,0 +1,21 @@
+﻿class ExitAction
+{
+    public static void Exit()
+    {
+        Console.Clear();
+        string act = ConsoleManager.GetInput<string>("Are you sure you want to exit (Y/N): ",ConsoleColor.White);
+
+        string action = act.ToLower();
+
+        if (act == "y")
+        {
+            Environment.Exit(0);
+        }
+        else if (act == "n")
+        {
+            return;
+        }
+
+        ConsoleManager.WriteColored("\n❌ The transaction you want to make is invalid! Please try again.", ConsoleColor.Red);
+    }
+}
