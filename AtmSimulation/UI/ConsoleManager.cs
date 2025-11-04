b3658cbf9c50881553ab3244809c0c2ac2304db1
Kaaner <kaaner4mir@gmail.com>
2025-11-04 09:33:09 +0300

    feat: add spinner animation

--- AtmSimulation/UI/ConsoleManager.cs
+++ AtmSimulation/UI/ConsoleManager.cs
@@ -63,4 +63,27 @@
         Console.CursorVisible = true;
     }
 
+    public static void SpinnerAnimation()
+    {
+        char[] items = { '-', '\\', '|', '/' };
+
+        byte loopTime = 50;
+        byte loopCount = 0;
+
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        while (loopCount<5)
+        {
+            foreach (var item in items)
+            {
+                Console.Write($"\r{item}");
+                Thread.Sleep(loopTime);
+            }
+            loopCount++;
+        }
+        Console.CursorVisible = true;
+        Console.Clear();
+    }
+
 }
