0fb18160619605e4ff6e66455a760dceadcd6a33
Kaaner <kaaner4mir@gmail.com>
2025-11-07 21:10:32 +0300

    feat: add vehicle data

--- RentingCarSystem/Database/Data.cs
+++ RentingCarSystem/Database/Data.cs
@@ -1,6 +1,6 @@
 ﻿class Data
 {
-    public static List<Car> vehicles = new List<Car>()
+    public static List<Car> cars = new List<Car>()
     {
         new Car()
         {
@@ -56,5 +56,152 @@
             Color="Red",
             HP=140,
         }
+    };
+
+    public static List<Bus> buses = new List<Bus>()
+    {
+        new Bus()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Mercedez",
+            Model="O302",
+            Year=2015,
+            Color="White",
+            Capacity=65
+        },
+        new Bus()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="MAN",
+            Model="solo-CNG",
+            Year=2014,
+            Color="Gray",
+            Capacity=50
+        },
+        new Bus()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Temsa",
+            Model="Avenue",
+            Year=2020,
+            Color="Black",
+            Capacity=80
+        },
+        new Bus()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Iveco",
+            Model="Crossway",
+            Year=2024,
+            Color="White",
+            Capacity=90
+        },
+        new Bus()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="BMC",
+            Model="PROCITY",
+            Year=2015,
+            Color="Yellow",
+            Capacity=52
+        }
+    };
+
+    public static List<Commercial> commercials = new List<Commercial>()
+    {
+        new Commercial()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Mercedez-Benz",
+            Model="Sprinter",
+            Year=2025,
+            Color="Black",
+            Area="School Service"
+        },
+        new Commercial()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Maxus",
+            Model="V90",
+            Year=2024,
+            Color="Black",
+            Area="Working Service"
+        },
+        new Commercial()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Fiat",
+            Model="Ducato",
+            Year=2023,
+            Color="Red",
+            Area="Merchant"
+        },
+        new Commercial()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Mercedez-Benz",
+            Model="Sprinter",
+            Year=2025,
+            Color="Black",
+            Area="School"
+        },
+        new Commercial()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Mercedez-Benz",
+            Model="Vito",
+            Year=2025,
+            Color="Piano Black",
+            Area="VIP"
+        },
+    };
+
+    public static List<Motocycle> motocycles = new List<Motocycle>()
+    {
+        new Motocycle()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Ducati",
+            Model="Panigale V4",
+            Year=2024,
+            Color="Red",
+            CC=1000
+        },
+        new Motocycle()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Yamaha",
+            Model="R1",
+            Year=2025,
+            Color="Blue",
+            CC=1000
+        },
+        new Motocycle()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Aprilla",
+            Model="RS 457",
+            Year=2024,
+            Color="Red",
+            CC=450
+        },
+        new Motocycle()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Honda",
+            Model="1000RR",
+            Year=2022,
+            Color="White",
+            CC=1000
+        },
+        new Motocycle()
+        {
+            VehicleId= Random.Shared.Next(100000,999999),
+            Brand="Yamaha",
+            Model="XMAX",
+            Year=2024,
+            Color="Black",
+            CC=600
+        },
     };
 }
